4bdf924c56eb9126167d00b530303b8ffdbeeb9a
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-20 22:35:01 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,7 +6,7 @@ import os
 import sys
 import asyncio 
 from database import Db, db
-from config import Config, temp
+from config import Config, AUTH_CHANNEL, temp
 from script import Script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
