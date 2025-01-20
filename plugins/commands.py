27c4e9561004b4faea107290ef4ae708fcc0556d
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-20 22:31:36 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,6 +10,7 @@ from config import Config, temp
 from script import Script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
+from pyrogram.errors import *
 import psutil
 import time as time
 from os import environ, execle, system
@@ -38,6 +39,18 @@ main_buttons = [[
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
+    return btn
+    
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     if AUTH_CHANNEL:
