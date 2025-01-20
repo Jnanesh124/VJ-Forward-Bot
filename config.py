9ee348b34ebd9bdce44f1e677e9eda338e71d4e2
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-20 22:32:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-    AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+    AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1001764441595 -1002135593873').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
